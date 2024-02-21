578204352224f62b9a6604baab4365da228a016c
thehamzakargin <hamzakargin@outlook.com>
2024-02-21 10:17:27 +0300

    dispatch finished

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,5 +1,10 @@
+import { useSelector } from "react-redux";
+
 function CarValue() {
-    return <div>Car Value</div>
+    const totalCost = useSelector(({car: {data, searchTerm}}) => 
+        data.filter((car) =>car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        ).reduce((acc, car) => acc + car.cost, 0));
+    return <div className="car-value">total cost: ${totalCost}</div>;
 }
 
 export default CarValue;
